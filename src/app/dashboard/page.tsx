a44ee32ff4a881770def50547c056891b4d3490f
AlvaroNascimento94 <alvarosum@hotmail.com>
2025-10-18 18:07:00 -0300

    refactor: adicionar estrutura e estilo ao componente Dashboard com carregamento de pedidos

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,11 +1,68 @@
-import { Orders } from "./componentes/orders";
+'use client'
+
+import { Orders } from "./orders";
 import style from "./dashboard.module.scss"
+import { Plus } from "lucide-react"
+import { useRouter } from "next/navigation"
+import { useState, useEffect } from "react";
+import { api } from "@/services/api";
+import { getCookieCliente } from "@/lib/cookieClient";
 
 export default function Dashboard() {
-    return (<>
-        <main className={style.container}>
+    const [orders, setOrders] = useState([]);
+    const [loading, setLoading] = useState(true);
 
-            <Orders />
+    const router = useRouter()
+
+    async function handleCreate() {
+        router.replace("/dashboard/order")
+    }
+
+    useEffect(() => {
+        async function loadOrders() {
+            try {
+                const token = getCookieCliente();
+
+                const response = await api.get("/orders", {
+                    headers: {
+                        Authorization: `Bearer ${token}`
+                    }
+                });
+                setOrders(response.data);
+                console.log("Pedidos carregados:", response.data);
+            } catch (error) {
+                console.error("Erro ao carregar pedidos:", error);
+            } finally {
+                setLoading(false);
+            }
+
+        }
+        loadOrders();
+    }, []);
+
+    return (
+        <main className={style.container}>
+            <section className={style.body}>
+
+                <section className={style.containerHeader}>
+                    <h1>Últimos Pedidos</h1>
+                    <button onClick={handleCreate}>
+                        <Plus />
+                        Novo Pedido
+                    </button>
+                </section>
+                <section className={style.containerBody}>
+
+                    {loading && <p>Carregando pedidos...</p>}
+
+                    {!loading && orders.length > 0 ? (
+                        orders.map((order) =>
+                            <Orders order={order} key={order} />)
+                    ) : (
+                        !loading && <p>Nenhum pedido encontrado</p>
+                    )}
+                </section>
+            </section>
         </main>
-    </>);
+    );
 }
